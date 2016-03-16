e89d8031fd81acc8dc95418865f56bd348ce45ab
Viktor Elofsson <viktor@viktorelofsson.se>
2016-03-16 18:29:55 +0000

    Add multiline textbox to support IPv6 addresses.

--- include/picotorrent/core/string_operations.hpp
+++ include/picotorrent/core/string_operations.hpp
@@ -2,12 +2,15 @@
 
 #include <picotorrent/common.hpp>
 #include <string>
+#include <vector>
 
 namespace picotorrent
 {
 namespace core
 {
+    DLL_EXPORT std::wstring join(const std::vector<std::wstring> &list, const std::wstring &delimiter);
     DLL_EXPORT std::string to_string(const std::wstring &str);
     DLL_EXPORT std::wstring to_wstring(const std::string &str);
+    DLL_EXPORT std::wstring trim(const std::wstring &str);
 }
 }
