6bce0feba772997ab1c462e41c5a19b711b61569
Viktor Elofsson <viktor@viktorelofsson.se>
2017-06-07 16:04:49 +0200

    Save configuration to disk when closing the Preferences dialog (#433)

--- src/Controllers/ViewPreferencesController.cpp
+++ src/Controllers/ViewPreferencesController.cpp
@@ -2,6 +2,7 @@
 
 #include <libtorrent/session.hpp>
 
+#include "../Configuration.hpp"
 #include "../Core/SessionSettings.hpp"
 #include "../PropertySheets/Preferences/PreferencesSheet.hpp"
 #include "../resources.h"
@@ -22,6 +23,10 @@ void ViewPreferencesController::Execute()
 {
     PropertySheets::Preferences::PreferencesSheet sheet(m_notifyIcon);
     sheet.DoModal();
+
+    // Save configuration to disk
+    Configuration& cfg = Configuration::GetInstance();
+    cfg.Save();
 
     lt::settings_pack settings = Core::SessionSettings::Get();
     m_session->apply_settings(settings);
