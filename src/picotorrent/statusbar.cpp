73d550549c9fac569dfd4280892f71b72b55d08d
Viktor Elofsson <viktor@viktorelofsson.se>
2018-04-23 20:07:29 +0200

    Move strings to translation files

--- src/picotorrent/statusbar.cpp
+++ src/picotorrent/statusbar.cpp
@@ -1,11 +1,13 @@
 #include "statusbar.hpp"
 
+#include "translator.hpp"
 #include "utils.hpp"
 
 using pt::StatusBar;
 
-StatusBar::StatusBar(wxWindow* parent)
-    : wxStatusBar(parent, wxID_ANY)
+StatusBar::StatusBar(wxWindow* parent, std::shared_ptr<pt::Translator> translator)
+    : wxStatusBar(parent, wxID_ANY),
+    m_translator(translator)
 {
     int widths[] =
     {
@@ -22,23 +24,23 @@ void StatusBar::UpdateDhtNodesCount(int64_t nodes)
 {
     if (nodes < 0)
     {
-        SetStatusText("DHT: disabled", 1);
+        SetStatusText(i18n(m_translator, "dht_disabled"), 1);
     }
     else
     {
-        SetStatusText(wxString::Format("DHT: %I64d node(s)", nodes), 1);
+        SetStatusText(wxString::Format(i18n(m_translator, "dht_i64d_nodes"), nodes), 1);
     }
 }
 
 void StatusBar::UpdateTorrentCount(int64_t torrents)
 {
-    SetStatusText(wxString::Format("%I64d torrent(s)", torrents), 0);
+    SetStatusText(wxString::Format(i18n(m_translator, "i64d_torrents"), torrents), 0);
 }
 
 void StatusBar::UpdateTransferRates(int64_t downSpeed, int64_t upSpeed)
 {
     SetStatusText(
-        wxString::Format("DL: %s/s, UL: %s/s",
+        wxString::Format(i18n(m_translator, "dl_s_ul_s"),
             Utils::ToHumanFileSize(downSpeed),
             Utils::ToHumanFileSize(upSpeed)),
         2);
