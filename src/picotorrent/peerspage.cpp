e5e69c77414940d27faa26d3ba44a9b46fc0c956
yhnmj6666 <cai.sky.china@gmail.com>
2019-01-03 01:18:29 +0800

    add sort on peers page

--- src/picotorrent/peerspage.cpp
+++ src/picotorrent/peerspage.cpp
@@ -13,19 +13,9 @@ using pt::PeersPage;
 
 PeersPage::PeersPage(wxWindow* parent, wxWindowID id, std::shared_ptr<pt::Translator> tr)
     : wxPanel(parent, id),
-    m_peersView(new wxDataViewCtrl(this, wxID_ANY)),
+    m_peersView(new PeersListView(this, ptID_TORRENT_LIST_VIEW, tr)),
     m_viewModel(new PeersViewModel())
 {
-    m_peersView->AppendTextColumn(i18n(tr, "ip"), PeersViewModel::Columns::IP, wxDATAVIEW_CELL_INERT, SX(110));
-    m_peersView->AppendTextColumn(i18n(tr, "client"), PeersViewModel::Columns::Client, wxDATAVIEW_CELL_INERT, SX(140));
-    m_peersView->AppendTextColumn(i18n(tr, "flags"), PeersViewModel::Columns::Flags, wxDATAVIEW_CELL_INERT, SX(80));
-    m_peersView->AppendTextColumn(i18n(tr, "dl"), PeersViewModel::Columns::DownloadRate, wxDATAVIEW_CELL_INERT, SX(80), wxALIGN_RIGHT);
-    m_peersView->AppendTextColumn(i18n(tr, "ul"), PeersViewModel::Columns::UploadRate, wxDATAVIEW_CELL_INERT, SX(80), wxALIGN_RIGHT);
-    m_peersView->AppendProgressColumn(i18n(tr, "progress"), PeersViewModel::Columns::Progress, wxDATAVIEW_CELL_INERT, SX(100));
-
-    // Ugly hack to prevent the last "real" column from stretching.
-    m_peersView->AppendColumn(new wxDataViewColumn(wxEmptyString, new wxDataViewTextRenderer(), -1, 0));
-
     m_peersView->AssociateModel(m_viewModel);
     m_viewModel->DecRef();
 
@@ -43,4 +33,5 @@ void PeersPage::Clear()
 void PeersPage::Update(lt::torrent_status const& ts)
 {
     m_viewModel->Update(ts);
+    m_peersView->Sort();
 }
